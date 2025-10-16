2d0057cdf789daf569cc7eb8a08914ae6d78f65e
Rojasito <159412387+Rojasito@users.noreply.github.com>
2025-10-16 02:59:39 -0300

    Add files via upload

---
+++ Ejercicio-06/php/encabezado.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<link rel="stylesheet" href="bootstrap-5.3.7-dist/css/bootstrap.min.css">
+</head>
+<body class="bg-danger">
+<header>
+	<h1 class="bg-dark text-info text-center p-4">Laboratorio ll</h1>
+</header>
+<nav class="navbar bg-dark">
+	<ul class="nav container">
+		<li class="btn nav-item bg-info"><a class="nav-link text-dark" href="../Ejercicio-01/index.php">Ejercicio 1</a></li>
+		<li class="btn nav-item bg-info"><a class="nav-link text-dark" href="#">Ejercicio 2</a></li>
+		<li class="btn nav-item bg-info"><a class="nav-link text-dark" href="../Ejercicio-03/index.php">Ejercicio 3</a></li>
+		<li class="btn nav-item bg-info"><a class="nav-link text-dark" href="../Ejercicio-04/index.php">Ejercicio 4</a></li>
+		<li class="btn nav-item bg-info"><a class="nav-link text-dark" href="../Ejercicio-05/index.php">Ejercicio 5</a></li>
+		<li class="btn nav-item bg-info"><a class="nav-link text-dark" href="../Ejercicio-06/index.php">Ejercicio 6</a></li>
+	</ul>
+</nav>
